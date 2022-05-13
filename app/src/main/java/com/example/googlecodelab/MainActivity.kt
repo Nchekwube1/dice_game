4d762df3ef99513402fe525091bbb9cc2575e2ad
Unekwe Francis <unekwenchekwube@gmail.com>
2022-05-13 21:11:02 +0100

    added second randow dice image view

--- app/src/main/java/com/example/googlecodelab/MainActivity.kt
+++ app/src/main/java/com/example/googlecodelab/MainActivity.kt
@@ -8,18 +8,32 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
             private lateinit var diceImage: ImageView
+            private lateinit var diceImage2: ImageView
 
             override fun onCreate(savedInstanceState: Bundle?) {
                         super.onCreate(savedInstanceState)
                         setContentView(R.layout.activity_main)
 
+                        diceImage = findViewById(R.id.iv_diceImage)
+                        diceImage2 = findViewById(R.id.iv_diceImage2)
                         val btnRoll: Button = findViewById(R.id.btn_roll)
                         btnRoll.setOnClickListener { rollDice() }
+
             }
 
             private fun rollDice() {
-                        diceImage = findViewById(R.id.iv_diceImage)
+//                        diceImage2 = findViewById(R.id.iv)
                         val drawableResource =  when((1..6).random()){
+                                 1 -> R.drawable.dice_1
+                                 2 ->  R.drawable.dice_2
+                                 3 -> R.drawable.dice_3
+                                 4 -> R.drawable.dice_4
+                                 5 -> R.drawable.dice_5
+                                 6 -> R.drawable.dice_6
+                                 else-> R.drawable.empty_dice
+
+                        }
+                        val drawableResource2 =  when((1..6).random()){
                                  1 -> R.drawable.dice_1
                                  2 ->  R.drawable.dice_2
                                  3 -> R.drawable.dice_3
@@ -31,6 +45,7 @@ class MainActivity : AppCompatActivity() {
                         }
 
                         diceImage.setImageResource(drawableResource)
+                        diceImage2.setImageResource(drawableResource2)
             }
 
 }
